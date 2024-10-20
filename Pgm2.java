651b8948821a65d85f4ba6f25518031d3bfbdbb1
shreyanka <shreyankabhargav@gmail.com>
2024-10-20 10:37:50 +0530

    Program2

--- Pgm2.java
+++ Pgm2.java
@@ -5,9 +5,17 @@ public class Pgm2{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number");
         num=sc.nextInt();
+        System.out.println("Tables in forward order is");
         for (i=1;i<=10;i++)
         {
             System.out.println(num+"X"+i+"="+i*num);
         }
+        System.out.println("\n");
+        System.out.println("Tables in reverse order is");
+        for (i=10;i>=1;i--)
+        {
+            System.out.println(num+"X"+i+"="+i*num);
+        }
+        
     }
 }
