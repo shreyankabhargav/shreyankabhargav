b94f50bc4771f0461a4ba0d34ed572e5e0d118d2
shreyanka <shreyankabhargav@gmail.com>
2024-10-21 19:14:42 +0530

    Program5

---
+++ Pgm5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Pgm5{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter marks ");
+        int marks = scanner.nextInt();
+        if (marks >= 0 && marks <= 100) 
+        {
+            String grade = method(marks);
+            System.out.println("Marks= " + marks + " Grade= " + grade);
+        } 
+        else 
+        {
+            System.out.println("Invalid input");
+        }
+    }
+        static String method(int marks) {
+        if (marks >= 91 && marks <= 100) {
+            return "AA";
+        } else if (marks >= 81 && marks <= 90) {
+            return "AB";
+        } else if (marks >= 71 && marks <= 80) {
+            return "BB";
+        } else if (marks >= 61 && marks <= 70) {
+            return "BC";
+        } else if (marks >= 51 && marks <= 60) {
+            return "CD";
+        } else if (marks >= 41 && marks <= 50) {
+            return "DD";
+        } else {
+            return "Fail";
+        }
+    }
+
+   
+}
+
