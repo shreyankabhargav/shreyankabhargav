e522a052d4ee5ead638bf1ea641f184dcaa3a973
shreyanka <shreyankabhargav@gmail.com>
2024-10-22 10:09:58 +0530

    Program5

--- Pgm5.java
+++ Pgm5.java
@@ -1,37 +1,14 @@
 import java.util.Scanner;
-public class Pgm5{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter marks ");
-        int marks = scanner.nextInt();
-        if (marks >= 0 && marks <= 100) 
-        {
-            String grade = method(marks);
-            System.out.println("Marks= " + marks + " Grade= " + grade);
-        } 
-        else 
-        {
-            System.out.println("Invalid input");
-        }
+public class Pgm5 {
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        System.out.println("Sum of "+ n + " natural numbers is" +function(n));
     }
-        static String method(int marks) {
-        if (marks >= 91 && marks <= 100) {
-            return "AA";
-        } else if (marks >= 81 && marks <= 90) {
-            return "AB";
-        } else if (marks >= 71 && marks <= 80) {
-            return "BB";
-        } else if (marks >= 61 && marks <= 70) {
-            return "BC";
-        } else if (marks >= 51 && marks <= 60) {
-            return "CD";
-        } else if (marks >= 41 && marks <= 50) {
-            return "DD";
-        } else {
-            return "Fail";
-        }
+    public static int function(int n){
+        return n*(n+1)/2;
     }
-
-   
+    
 }
-
